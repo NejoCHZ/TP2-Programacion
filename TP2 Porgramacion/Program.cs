3b4adc910686868fdb389135bf22b124b04c8e33
NejoCHZ <169078002+NejoCHZ@users.noreply.github.com>
2024-08-15 01:50:59 -0300

    Add files via upload

--- TP2 Porgramacion/Program.cs
+++ TP2 Porgramacion/Program.cs
@@ -19,43 +19,21 @@ namespace TP2_Porgramacion
              //7Salir del sistema(ingreso opcion 0)
              //1
                 string vueloNumero1 = "1";
-                string vueloOrigen1 = "Córdoba";
-                string vueloDestino1 = "Buenos Aires (Ezeiza)";
-                string horaSalida1 = "21:50";
-                string HoraLlegada1 = "23:10";
                 double vueloPrecio1 = 43700.00;
 
                 string vueloNumero2 = "2";
-                string vueloOrigen2 = "Buenos Aires(Ezeiza)";
-                string vueloDestino2 = "Córdoba";
                 double vueloPrecio2 = 43700.00;
 
                 string vueloNumero3 = "3";
-                string vueloOrigen3 = "Córdoba";
-                string vueloDestino3 = "Mendoza";
-                string horaSalida3 = "16:45";
-                string HoraLlegada3 = "17:55";
                 double vueloPrecio3 = 52000.00;
 
                 string vueloNumero4 = "4";
-                string vuelorigen4 = "Mendoza";
-                string vueloDestino4 = "Córdoba";
-                string horaSalida4 = "21:50";
-                string HoraLlegada4 = "23:10";
                 double vueloPrecio4 = 52000.00;
 
                 string vueloNumero5 = "5";
-                string vueloOrigen5 = "Córdoba";
-                string vueloDestino5 = "Bariloche";
-                string horaSalida5 = "21:50";
-                string horaLlegada5 = "7:35";
                 double vueloPrecio5 = 183000.00;
 
                 string vueloNumero6 = "6";
-                string vueloOrigen6 = "Bariloche";
-                string vueloDestino6 = "Córdoba"; 
-                string horaSalida6 = "13:00";
-                string horaLlegada6 = "15:00";
                 double vueloPrecio6 = 183000.00;
 
 
@@ -63,7 +41,15 @@ namespace TP2_Porgramacion
                 bool salir = false;
 
                 while (!salir)
-                {
+                { Console.WriteLine("Bienvenido al menú de opciones, ingrese el numero correspondiente a lo que necesita" +
+                    "1.Ver vuelos disponibles." +
+                    "2.Agregar al carrito" +
+                    "3.Pagar pasajes" +
+                    "4.Registrarse" +
+                    "5.Iniciar Sesión." +
+                    "6.Cerrar Sesión" +
+                    "0.Salir del sistema");
+               int opcionUsuario = int.Parse (Console.ReadLine());
                     switch (opcionUsuario)
                     {
                         case 0:
@@ -72,16 +58,16 @@ namespace TP2_Porgramacion
                             break;
                         case 1:
                         Console.WriteLine("Vuelos Disponibles:" +
-                            "Nro de vuelo:1. Origen: Córdoba. Destino: Buenos Aires (Ezeiza). Salida: 21:50. Llegada: 23:10. Precio: $43700,00"+
-                            "Nro de vuelo:2. Origen: Buenos Aires (Ezeiza). Destino: Córdoba. Salida: 19:50. Llegada: 21:10. Precio: $43700,00"+
-                            "Nro de vuelo:3. Origen: Córdoba. Destino: Mendoza. Salida: 21:50. Llegada: 23:10. Precio: $43700,00" +
-                            "Nro de vuelo:4. Origen: Córdoba. Destino: Buenos Aires (Ezeiza). Salida: 21:50. Llegada: 23:10. Precio: $43700,00" +
+                            "Nro de vuelo:1. Origen: Córdoba. Destino: Buenos Aires (Ezeiza). Salida: 21:50. Llegada: 23:10. Precio: $43700,00" +
+                            "Nro de vuelo:2. Origen: Buenos Aires (Ezeiza). Destino: Córdoba. Salida: 19:50. Llegada: 21:10. Precio: $43700,00" +
+                            "Nro de vuelo:3. Origen: Córdoba. Destino: Mendoza. Salida: 16:45. Llegada: 17:55. Precio: $52000,00" +
+                            "Nro de vuelo:4. Origen: Mendoza. Destino: Córdoba. Salida: 21:50. Llegada: 23:10. Precio: $52000,00" +
+                            "Nro de vuelo:5. Origen: Córdoba. Destino: Bariloche. Salida: 21:50. Llegada: 7:35. Precio: $183000,00" +
+                            "Nro de vuelo:6. Origen: Bariloche. Destino: Córdoba. Salida: 13:00. Llegada: 15:00. Precio: $183000,00");
 
-
-
-                            break;
+                            continue;
                         case 2:
-                        Console.WriteLine("ingrese numero de vuelo");
+                        Console.WriteLine("ingrese numero de vuelo que desea");
                         int numeroDeVuelo = int.Parse(Console.ReadLine()); 
 
                             while (numeroDeVuelo > 0 && numeroDeVuelo < 6)
@@ -89,7 +75,19 @@ namespace TP2_Porgramacion
                                
                                 Console.WriteLine("ingresar la fecha del vuelo");
                                 int fechaViaje = int.Parse(Console.ReadLine());
-                                Console.WriteLine("ingresar cantidad de pasajes");
+                            Console.Write("Ingresa una fecha en el formato dd/mm/YYYY: ");
+                            string fecha = Console.ReadLine();
+                            DateTime fechaIngresada = DateTime.Parse(fecha);
+                            DateTime fechaActual = DateTime.Now;
+                            if (fechaIngresada > fechaActual)
+                            {
+                                Console.WriteLine("La fecha ingresada es mayor a la actual");
+                            }
+                            else
+                            {
+                                Console.WriteLine("La fecha ingresada es menor a la actual");
+                            }
+                            Console.WriteLine("ingresar cantidad de pasajes");
                                 int cantidadPasajes = int.Parse(Console.ReadLine());
 
 
@@ -115,15 +113,10 @@ namespace TP2_Porgramacion
                     }
                 }
 
-            }
-            static void mostrarVuelos()
-            {
-
-            }
-
-
-        }
-
+          }
     }
-    }
-}
+    
+
+    
+    
+
